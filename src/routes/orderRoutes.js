38423a2d506a1dadf8745b6a81042c97e01b003d
KanokphonTeepang <kanokphon.teep@bumail.net>
2025-12-04 15:02:23 +0700

    DeleteUnusedFiles

--- src/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createOrder } from '../controllers/orderController.js'; // อย่าลืม .js
-
-const router = express.Router();
-
-router.post('/', createOrder);
-
-export default router;
